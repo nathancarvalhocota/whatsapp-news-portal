7d5b467bafa0a06124fe1d6b2bc88867fb8de234
Nathan Carvalho <nathancarvalhocota@gmail.com>
2026-03-29 19:44:02 -0300

    fix: default postgres port to 5432 when not present in Render URI

--- apps/api-dotnet/WhatsAppNewsPortal.Api/Program.cs
+++ apps/api-dotnet/WhatsAppNewsPortal.Api/Program.cs
@@ -50,7 +50,8 @@ if (connectionString.StartsWith("postgres://") || connectionString.StartsWith("p
 {
     var uri = new Uri(connectionString);
     var userInfo = uri.UserInfo.Split(':');
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')}" +
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    connectionString = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')}" +
                        $";Username={userInfo[0]};Password={Uri.UnescapeDataString(userInfo[1])}" +
                        ";SSL Mode=Require;Trust Server Certificate=true";
 }
